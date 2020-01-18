412e306279270d49d2fa635551110e0b5edbd000
Placido Wang <placidow@gmail.com>
2020-01-18 10:15:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,8 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(nameses, language)
-  "Hi, my name is #{nameses} and I am learning to program in #{language}."
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
